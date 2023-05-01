64f001db959fd42fe066d14a0d234e579e729eaf
Joaco27 <joaco200127@gmail.com>
2023-05-01 19:42:54 -0300

    Agregue admin

--- OhMyDog/APP/admin.py
+++ OhMyDog/APP/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(Perro)
 
 # Register your models here.
--- OhMyDog/APP/models.py
+++ OhMyDog/APP/models.py
@@ -6,7 +6,8 @@ class Perro(models.Model):
     nombre = models.CharField(max_length=15)
     raza = models.CharField(max_length=30)
     edad = models.IntegerField()
-    dniDueño = models.IntegerField()
     color = models.CharField(max_length=15)
     
+    def __str__(self):
+        return f'Perro: {self.nombre} con {self.edad} años'
     
